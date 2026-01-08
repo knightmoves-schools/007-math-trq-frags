6925d6e4613f748d870e596eb2dca099b70b8326
track-frags <jadirks@anamosa.k12.ia.us>
2026-01-08 18:02:44 +0000

    bark bark ?????????????????????

--- Math.cs
+++ Math.cs
@@ -1,5 +1,11 @@
 ﻿namespace knightmoves;
 public class Math
 {
-    // Add your code here
+    public int total = 35;
+    public int Sum = 1 + 4;
+    public int Difference = 5 - 2;
+    public int Product = 3 * 5;
+    public int Quotient = 15 / 2;
+    public int Remainder = 7 % 2;
+    
 }
